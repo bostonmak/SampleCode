b907df740535828b739511a73c947d5ef7e494b3
bostonmak <boston@makotexas.com>
2016-08-07 22:46:29 -0700

    add overwatch and league of legends stats

--- TwitchBot.py
+++ TwitchBot.py
@@ -9,15 +9,26 @@ import BeautifulSoup
 import re
 import json
 import time
+from array import array
  
 # Set all the variables necessary to connect to Twitch IRC
 HOST = "irc.chat.twitch.tv"
 NICK = "bonanarama"
 PORT = 6667
-PASS = "oauth:OAUTH_KEY"
+PASS = "oauth:py38r9f6w72r1cmkv9ugbvuxbpajnw"
+RIOTAPI = "RGAPI-35C5ECF7-724A-446E-9255-B76D32726C5F"
+SUMMONERID = "34500916"
 readbuffer = ""
 MODT = False
 pingNum = 0
+
+
+cooldowns = [int(0) for i in range(4)];
+updateCooldown = 0
+cachedOverwatchLevel = 0
+cachedOverwatchRank = 0
+cachedRealmStats = 0
+cachedPetStats = 0
  
 # Connecting to Twitch IRC by passing credentials and joining a certain channel
 s = socket.socket()
@@ -32,33 +43,62 @@ def send_message(message):
 
 def get_stats(gameName):
     if gameName.lower() == 'League of Legends'.lower():
-        return 'Silver 1... i suck'        
+        return get_league_of_legends_stats() 
     if gameName.lower() == 'Realm of the Mad God'.lower() or gameName.lower() == 'rotmg':
-        session = requests.session()
-        req = session.get('https://www.realmeye.com/player/Bonanah')
-        doc = BeautifulSoup.BeautifulSoup(req.content)
-        stats = doc.find('span', { "class" : "player-stats" })
-        return format_realm_stats(stats)        
+        return format_realm_stats()        
     if gameName.lower() == 'Overwatch'.lower():
-        return 'The cute girl I am playing is 10/10'
+        global cachedOverwatchLevel
+        global cachedOverwatchRank
+        set_overwatch_stats()
+        if int(cachedOverwatchRank) < 40:
+            return "I am currently level " + str(cachedOverwatchLevel) + " with a competitve rank of " + str(cachedOverwatchRank) + ". Teach me how to be good please"
+        else:        
+            return "I am currently level " + str(cachedOverwatchLevel) + " with a competitve rank of " + str(cachedOverwatchRank)
 
-    return 'Could not find a game by the name of ' + gameName   
+    return 'Could not find a game by the name of ' + gameName + " or could not find any stats for it at this time."   
 
-def get_pet_stats(stats):
+def get_league_of_legends_stats():
+    req = requests.get("https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + SUMMONERID + "/entry?api_key=" + RIOTAPI)
+    soloData = req.json()[SUMMONERID][0]
+    tier = soloData["tier"]
+    division = soloData["entries"][0]["division"]
+    points = soloData["entries"][0]["leaguePoints"]
+    if tier.lower() == "silver" or tier.lower() == "bronze":
+        return "I am " + tier.title() + " " + division + " with " + str(points) + "LP. I'm pretty bad at this game."
+    else:
+        return "I am " + tier.title() + " " + division + " with " + str(points) + "LP."
 
-    stats = stats.contents[1].contents[0]
-    petName = str(stats.contents[1])
+def set_overwatch_stats():
+    global cachedOverwatchLevel
+    global cachedOverwatchRank
+    req = requests.get("https://playoverwatch.com/en-us/career/pc/us/Bonana-1364")
+    doc = BeautifulSoup.BeautifulSoup(req.content)
+    cachedOverwatchLevel = doc.find('div', {"class" : "player-level"}).div.contents[0]
+    cachedOverwatchRank = doc.find('div', { "class" : "competitive-rank"}).div.contents[0]
+
+def set_realm_stats():
+    req = requests.get('https://www.realmeye.com/player/Bonanah')
+    doc = BeautifulSoup.BeautifulSoup(req.content)
+    cachedRealmStats = doc.find('span', { "class" : "player-stats" })
+
+    req = requests.get('https://www.realmeye.com/pets-of/Bonanah')
+    doc = BeautifulSoup.BeautifulSoup(req.content)
+    cachedPetStats = doc.find('table', { "class" : "table table-striped tablesorter" })
+
+def get_pet_stats():
+    cachedPetStats = cachedPetStats.contents[1].contents[0]
+    petName = str(cachedPetStats.contents[1])
     petName = petName[4 : len(petName) - 5]
-    rarity = str(stats.contents[2])
+    rarity = str(cachedPetStats.contents[2])
     rarity = rarity[4 : len(rarity) - 5]
 
-    skillOne = str(stats.contents[5].contents)
+    skillOne = str(cachedPetStats.contents[5].contents)
     skillOne = skillOne[7 : len(skillOne) - 8]
 
-    skillTwo = str(stats.contents[7].contents)
+    skillTwo = str(cachedPetStats.contents[7].contents)
     skillTwo = skillTwo[7 : len(skillTwo) - 8]
 
-    skillThree = str(stats.contents[9].contents)
+    skillThree = str(cachedPetStats.contents[9].contents)
     skillThree = skillThree[7 : len(skillThree) - 8]
 
     skillOneLevel = "0"
@@ -76,41 +116,44 @@ def get_pet_stats(stats):
         skillTwo = skillTwo[match.end():]
         skillThree = skillThree[match.end():]
 
-        skillOneLevel = str(stats.contents[6].contents[0].contents)
+        skillOneLevel = str(cachedPetStats.contents[6].contents[0].contents)
         skillOneLevel = skillOneLevel[3 : len(skillOneLevel) - 2]
     elif disabledText.match(skillThree):
         match = disabledText.match(skillThree)
         skillThree = skillThree[match.end():]
         
-        skillOneLevel = str(stats.contents[6].contents[0].contents)
+        skillOneLevel = str(cachedPetStats.contents[6].contents[0].contents)
         skillOneLevel = skillOneLevel[3 : len(skillOneLevel) - 2]
-        skillTwoLevel = str(stats.contents[8].contents[0].contents)
+        skillTwoLevel = str(cachedPetStats.contents[8].contents[0].contents)
         skillTwoLevel = skillTwoLevel[3 : len(skillTwoLevel) - 2]
     else:
-        skillOneLevel = str(stats.contents[6].contents[0].contents)
+        skillOneLevel = str(cachedPetStats.contents[6].contents[0].contents)
         skillOneLevel = skillOneLevel[3 : len(skillOneLevel) - 2]
-        skillTwoLevel = str(stats.contents[8].contents[0].contents)
+        skillTwoLevel = str(cachedPetStats.contents[8].contents[0].contents)
         skillTwoLevel = skillTwoLevel[3 : len(skillTwoLevel) - 2]
-        skillThreeLevel = str(stats.contents[10].contents[0])
+        skillThreeLevel = str(cachedPetStats.contents[10].contents[0].contents)
         skillThreeLevel = skillThreeLevel[3 : len(skillThreeLevel) - 2]
+
+    print skillThreeLevel + ", " + skillThree;
 
     return "My {0} {1} has {2} {3}, {4} {5}, and {6} {7}".format(rarity, petName, skillOneLevel, skillOne, \
         skillTwoLevel, skillTwo, skillThreeLevel, skillThree)
 
-def format_realm_stats(stats):
-    baseStats = re.search("data-stats=\"\[[0-9,]*\]\"", str(stats))
-    statBonuses = re.search("data-bonuses=\"\[[0-9,]*\]\"", str(stats))
-    level = re.search("data-level=\"[0-9]*\"", str(stats))
-    if (stats == None or baseStats == None or statBonuses == None):
+def format_realm_stats():
+    req = requests.get('https://www.realmeye.com/player/Bonanah')
+    doc = BeautifulSoup.BeautifulSoup(req.content)
+    cachedRealmStats = doc.find('span', { "class" : "player-stats" })
+
+    baseStats = cachedRealmStats['data-stats']
+    statBonuses = cachedRealmStats['data-bonuses']
+    level = cachedRealmStats['data-level']
+    if (cachedRealmStats == None or baseStats == None or statBonuses == None):
         return 'Cannot access RealmEye stats at this time FeelsBadMan'
     #convert strings into arrays
-    baseStats = str(baseStats.group())[13 : len(str(baseStats.group())) - 2]
-    baseStats = baseStats.split(',')
-    statBonuses = str(statBonuses.group())[15 : len(str(statBonuses.group())) - 2]
-    statBonuses = statBonuses.split(',')
-    level = str(level.group())[12 : len(str(level.group())) - 1]
-    maxedStats = str(stats.contents)[3 : len(str(stats.contents)) - 2]
-    currentClass = stats.parent
+    baseStats = baseStats[1 : len(baseStats) - 1].split(',')
+    statBonuses = statBonuses[1 : len(statBonuses) - 1].split(',')
+    maxedStats = cachedRealmStats.contents[0]
+    currentClass = cachedRealmStats.parent
     totalStats = ["" for i in range(len(baseStats))]
     for index in range(0, len(baseStats)):
         if statBonuses[index] != '0':
@@ -125,7 +168,10 @@ def format_realm_stats(stats):
         maxedStats, currentClass, totalStats[0], totalStats[1], totalStats[2], totalStats[3], totalStats[4], \
         totalStats[5], totalStats[6], totalStats[7])
 
-cooldowns = [int(0) for i in range(4)];
+
+def setup():
+    set_overwatch_stats()
+    set_realm_stats()
  
 while True:
     readbuffer = readbuffer + s.recv(1024)
@@ -136,6 +182,7 @@ while True:
         # Checks whether the message is PING because its a method of Twitch to check if you're afk
         if ("PING :tmi.twitch.tv" in line):
             s.send("PONG {0}{1}\r\n".format(line[1],pingNum))
+            #print "ping {0}".format(pingNum)
             pingNum += 1
         else:
             # Splits the given string so we can work with it better
@@ -183,9 +230,13 @@ while True:
                     if cooldowns[2] + 5 <= currentTime and message.lower() == "!stats":
                         req = requests.get("https://api.twitch.tv/kraken/streams/bonanarama", headers={'Accept': 'application/vnd.twitchtv.v3+json'})
                         twitchJson = json.loads(req.content)
-                        stats = get_stats(twitchJson['stream']['game'])
-                        send_message(stats)
-                        print stats
+                        if twitchJson['stream'] == None:
+                            send_message("I am currently not online and therefore have no current game stats to give you")
+                            print "I am currently not online and therefore have no current game stats to give you"
+                        else:
+                            stats = get_stats(twitchJson['stream']['game'])
+                            send_message(stats)
+                            print stats
                         cooldowns[2] = currentTime
                     elif cooldowns[0] + 5 <= currentTime and re.match("!stats *", message):
                         stats = get_stats(message[7:])
@@ -201,6 +252,9 @@ while True:
                         send_message(petStats)
                         print petStats
                         cooldowns[3] = currentTime
+
+                    if message.lower() == "!test":
+                        get_overwatch_stats()
 
                 for l in parts:
                     if "End of /NAMES list" in l:
